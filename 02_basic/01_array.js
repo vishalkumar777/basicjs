06c1cbed97843c8be4a3799477189dd2450b124d
vishalkumar777 <vishalinfo97@gmail.com>
2025-05-14 06:54:02 +0000

    array

---
+++ 02_basic/01_array.js
@@ -0,0 +1,16 @@
+//array
+
+const myArr = [0,1,2,3,4,5]
+const myHero = ["shaktiman","naagraj"]
+
+const myArr2 = new Array(1,2,3,4)
+// console.log(myArr[2])
+
+// array methods
+
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+// myArr.unshift(9)
+// myArr.shift()
+console.log(myArr);
