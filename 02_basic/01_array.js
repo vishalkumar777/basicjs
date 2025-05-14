197378fb1c830e2af5de76f862e15db68d066394
vishalkumar777 <vishalinfo97@gmail.com>
2025-05-14 11:05:27 +0000

    array slice and splice

--- 02_basic/01_array.js
+++ 02_basic/01_array.js
@@ -13,4 +13,23 @@ const myArr2 = new Array(1,2,3,4)
 // myArr.pop();
 // myArr.unshift(9)
 // myArr.shift()
-console.log(myArr);
+// console.log(myArr);
+// console.log(myArr.includes(9)) // check 9 is available or not in array
+// console.log(myArr.indexOf(3))  // give index number is available and give -1 if not available
+
+const newArr = myArr.join() // change datatype array to string
+
+// console.log(myArr)
+// console.log(newArr)
+
+// slice and splice
+
+console.log("A ", myArr)
+
+const myArrN = myArr.slice(1,3)
+console.log(myArrN)
+console.log("B ", myArr);
+
+const myArrn2 = myArr.splice(1, 3) // remove orignal aray 1 to 3 index
+console.log("C ",myArr)
+console.log(myArr2)
