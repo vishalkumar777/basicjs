127e608ef52d4c3c868a485d9c81274a180dcbd7
vishalkumar777 <vishalinfo97@gmail.com>
2025-05-21 14:06:08 +0000

    map

--- 05_iterations/04_four.js
+++ 05_iterations/04_four.js
@@ -13,5 +13,17 @@ for (const key in myObject) {
 //array
 const programming = ['js','rb','py','java','cpp','php']
 for (const key in programming) {
-    console.log(programming[key])
+    // console.log(programming[key])
 }
+
+//map  map will not work becouse of map is not itratebal
+const map = new Map()
+map.set('IN', 'India')
+map.set('USA', 'United state of America')
+map.set('Fr','France')
+map.set('IN','India')
+
+for (const key in map) {
+    // console.log(key) // it will not work
+}
+
