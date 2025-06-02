fcf7e736205051b959f6782ff62fee3f62103b20
vishalkumar777 <vishalinfo97@gmail.com>
2025-06-02 09:44:32 +0000

    cls comment

--- 10_classes_and_oops/05_myClasses.js
+++ 10_classes_and_oops/05_myClasses.js
@@ -1,22 +1,22 @@
 //ES6
 
-class User {
-    constructor(username,email,password){
-        this.username=username;
-        this.email=email;
-        this.password=password;
-    }
+// class User {
+//     constructor(username,email,password){
+//         this.username=username;
+//         this.email=email;
+//         this.password=password;
+//     }
 
-    encryptPassword(){
-        return `${this.password}abc`
-    }
-    changeUsername(){
-        return `${this.username.toUpperCase()}`
-    }
-}
-const chai = new User("chai","chai@gmail.com","1213")
-console.log(chai.encryptPassword())
-console.log(chai.changeUsername())
+//     encryptPassword(){
+//         return `${this.password}abc`
+//     }
+//     changeUsername(){
+//         return `${this.username.toUpperCase()}`
+//     }
+// }
+// const chai = new User("chai","chai@gmail.com","1213")
+// console.log(chai.encryptPassword())
+// console.log(chai.changeUsername())
 
 //behind the scene
 
